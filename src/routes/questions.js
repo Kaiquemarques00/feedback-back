ed25d072724b476f3b0830e9468c6bb9ad0917fc
Kaique marques <kaiquemarques581@outlook.com>
2025-04-29 14:05:44 -0300

    Adicioando questions

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,6 +1,6 @@
-// routes/questions.js
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import authMiddleware from "../middleware/auth.js";
 
 const prisma = new PrismaClient();
 const router = express.Router();
@@ -14,6 +14,32 @@ router.post("/", async (req, res) => {
   });
 
   res.status(201).json(saved);
+});
+
+router.get("/", authMiddleware, async (req, res) => {
+  const page = parseInt(req.query.page) || 1; // Página atual (padrão é 1)
+  const limit = 5; // Número de dúvidas por página
+  const offset = (page - 1) * limit; // Deslocamento para a consulta
+
+  try {
+    const questions = await prisma.question.findMany({
+      skip: offset,
+      take: limit,
+      orderBy: { createdAt: "desc" }, // Ordena pela data de criação
+    });
+
+    // Contar o total de dúvidas para calcular o número total de páginas
+    const totalQuestions = await prisma.question.count();
+
+    res.json({
+      questions,
+      totalPages: Math.ceil(totalQuestions / limit), // Número total de páginas
+      currentPage: page, // Página atual
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao carregar dúvidas." });
+  }
 });
 
 export default router;
